9906655a5dec44685581629a8c4bdfe9b0219fa2
Logan <logan.cth@gmail.com>
2021-03-03 17:44:56 -0500

    Added Description

--- SierpinskiTriangle.py
+++ SierpinskiTriangle.py
@@ -3,6 +3,18 @@ from matplotlib.animation import FuncAnimation
 from numpy import sqrt
 from random import randint
 
+  GNU nano 3.2                       ReadMe.txt                                 
+
+#Here's what happens:
+#	1. Start in the middle of the triangle.
+#	2. Measure half the distance to a randomly chosen corner.
+#	3. Plot the point.
+#	4. Repeat from 2.
+
+#When prompted:
+#        Enter a natural number for the number of points.
+#        Enter an N or Y to choose an animationd or static plot.
+
 # the points are (0,0),(1,0),(0.5,1)
 xc = [0,0.5,0.25]
 yc = [0,0,0.5]
